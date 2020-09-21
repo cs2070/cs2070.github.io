2e3cb55355054b70a38da1a2e9b9d410492b55ee
cs2070 <61481699+cs2070@users.noreply.github.com>
2020-09-20 20:52:30 -0700

    edited for hw20

---
+++ Js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#savethewhales").hover(function(){
+      $(this).css("opacity", "0.8");
+    }, function(){
+    $("#savethewhales").css("opacity", "1");
+  });
+});
