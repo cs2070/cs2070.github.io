f1b8cb47d48f66b79e6d03d9765945401b8cfcf7
cs2070 <61481699+cs2070@users.noreply.github.com>
2020-10-10 14:02:06 -0700

    edited the website

--- Js/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("#savethewhales").hover(function(){
-      $(this).css("opacity", "0.8");
-    }, function(){
-    $("#savethewhales").css("opacity", "1");
-  });
-});
